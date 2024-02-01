bd79b816a703b989be640528414b5c62917f6ea8
Remy Sharp <remy@remysharp.com>
2024-02-01 16:49:45 +0000

    fix build so I can share a damn ts bug…

--- build.js
+++ build.js
@@ -1,38 +1,30 @@
 require('@remy/envy');
-const request = require('request');
-const write = require('fs').writeFile;
+const writeFileSync = require('fs').writeFileSync;
 
-const noop = error => error && console.log(error);
+const noop = (error) => error && console.log(error);
 
 function makeFile(short, body) {
-  const html = `<!doctype html><title>${
-    body.title
-  }</title><meta name="description" content="${
-    body.description
-  }"><link rel="shortcut icon" href="${
-    body.favicon
-  }"><meta http-equiv="refresh" content="0;${body.redirect}">`;
-  write(`./public/${short}.html`, html, noop);
+  const html = `<!doctype html><title>${body.title}</title><meta name="description" content="${body.description}"><link rel="shortcut icon" href="${body.favicon}"><meta http-equiv="refresh" content="0;${body.redirect}">`;
+  try {
+    writeFileSync(`./public/${short}.html`, html);
+  } catch (e) {
+    noop(e);
+  }
 }
 
-request(
-  {
-    url: `https://jsonbin.org/remy/urls/`,
-    json: true,
-  },
-  (error, res, body) => {
+fetch(`https://jsonbin.org/remy/urls/`)
+  .then((_) => _.json())
+  .then((body) => {
     let redirects = Object.entries(body)
       .filter(([short, target]) => {
         if (typeof target === 'string') {
           return true;
         }
-
         makeFile(short, target);
         return false;
       })
       .map(([short, url]) => `/${short}${' '.repeat(5)}${url}`)
       .join('\n');
 
-    write('./public/_redirects', redirects);
-  }
-);
+    writeFileSync('./public/_redirects', redirects);
+  });
